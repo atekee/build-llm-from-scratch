9b5bafc5e37d45928ad6da366e5698db4ff51873
malibayram <malibayram20@gmail.com>
2025-06-24 12:12:52 +0300

    Implement top-p filtering and enhance the generate method in UstaModel for improved token sampling. Adjust parameters for temperature, top_k, and top_p to refine output generation process.

--- v2/usta_model.py
+++ v2/usta_model.py
@@ -32,15 +32,49 @@ class UstaModel(nn.Module):
   max_prob, max_index, probs
   """
 
-  def generate(self, x: torch.Tensor, max_new_tokens: int): # top_k, top_p, temperature
-    tokens = x.detach().cpu().numpy().tolist()
+  def top_p_filtering(self, logits, top_p):
+    sorted_logits, sorted_indices = torch.sort(logits, descending=True)
+    cumulative_probs = torch.cumsum(torch.softmax(sorted_logits, dim=-1), dim=-1)
+    sorted_indices_to_remove = cumulative_probs > top_p
+    sorted_indices_to_remove[..., 1:] = sorted_indices_to_remove[..., :-1].clone()
+    sorted_indices_to_remove[..., 0] = False
+    
+    sorted_logits[sorted_indices_to_remove] = -float('inf')
+    filtered_logits = sorted_logits.clone()
+    filtered_logits.scatter_(0, sorted_indices, sorted_logits)
+    return filtered_logits
+
+
+
+  def generate(self, 
+               x: torch.Tensor,
+               max_new_tokens: int = 3,
+               temperature: float = 1.0,
+               top_k: int = 64,
+               top_p: float = 1.0
+              ): # top_k, top_p, temperature
+    tokens = x.tolist()
     
     for _ in range(max_new_tokens):
       x = x.unsqueeze(0).to(self.device)
       out = self.forward(x)
       out = out.squeeze(0)
-      probs = torch.softmax(out[-1], dim=-1)
-      _, max_index = torch.max(probs, dim=-1)
+      logits = out[-1]
+      if top_k > 0:
+        values, indexes = torch.topk(logits, k=top_k)
+        logits = torch.full_like(logits, -float('inf'))
+        logits.scatter_(0, indexes, values)
+
+      if top_p > 0 and top_p < 1:
+        logits = self.top_p_filtering(logits, top_p)
+
+      if temperature != 1.0 and temperature > 0:
+        logits = logits / temperature
+      
+      probs = torch.softmax(values, dim=-1)
+      # _, max_index = torch.max(probs, dim=-1)
+      sample = torch.multinomial(probs, 1)
+      max_index = indexes[sample]
       tokens.append(max_index.item())
       if max_index == 59 or len(tokens) > 32: # <eos> and max context length
         break
